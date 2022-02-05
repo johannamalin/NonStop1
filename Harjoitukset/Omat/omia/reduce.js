4fa929c70329295387b1e7bbe8f99a08af7f64e5
johannamalin <78158078+johannamalin@users.noreply.github.com>
2022-02-05 15:04:22 +0200

    selvennys

--- Harjoitukset/Omat/omia/reduce.js
+++ Harjoitukset/Omat/omia/reduce.js
@@ -1,3 +1,4 @@
+/*
 const newNumbers = [1, 3, 5, 7];
 
 const newSum = newNumbers.reduce((accumulator, currentValue) => {
@@ -7,3 +8,18 @@ const newSum = newNumbers.reduce((accumulator, currentValue) => {
 }, 10);
 
 console.log(newSum);
+*/
+
+// tyhmälle selvennys alempaan
+// a = 0, c = 1 => a = 1
+// a = 1, c = -1 => a = 0
+// a = 0, c = 2 => a = 2
+// a = 2, c = 3 => a = 5
+
+const numbers = [1, -1, 2, 3];
+
+const sum = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
+
+console.log(sum);
